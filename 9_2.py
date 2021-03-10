0e361f31bf99a8b4f4c9a5394828063b9b82dd9c
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-03-10 13:17:21 +0100

    add some changes

--- 9_2.py
+++ 9_2.py
@@ -3,9 +3,18 @@ import csv
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+rates = data[0].get('rates')
+# print(rates) to jest lista, ktorej elementami sa slowniki
+
+for r in rates:
+    print(r) #wylistowala slowniki
+
+for key, value in r.items():
+    print(r.get(key)) #OK dla ostatniej waluty, chociaz nie wiem czemu key a nie value
 
 fields = ['Currency', 'Code', 'Bid', 'Ask']
-rows = data[0]
+rows = r
+
 
 filename = "currency.csv"
 with open(filename, 'w') as csvfile:
